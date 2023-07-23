2e37bbefb2cc09934796c952571a77cebc7ab19a
EdrissAria <edrissalokozay77@gmail.com>
2023-07-23 10:19:20 -0400

    add register user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import User from "App/Models/User";
 import xmpp from "../../../xmpp-server";
 import { v4 as uuidv4 } from "uuid";
-import Env from '@ioc:Adonis/Core/Env'
+import Env from "@ioc:Adonis/Core/Env";
 
 export default class UsersController {
   async createUser({ request, response }: HttpContextContract) {
@@ -20,7 +20,42 @@ export default class UsersController {
 
     if (user) {
       //add client in ejabberd
-  
+
+      xmpp.on("online", () => {
+        console.log("online....");
+        const iq = new xmpp.Element("stream:stream", {
+          xmlns: "jabber:component:accept",
+          "xmlns:stream": "http://etherx.jabber.org/streams",
+          to: "localhost",
+        })
+          .c("iq", {
+            type: "set",
+            id: "reg2",
+          })
+          .c("query", {
+            xmlns: "jabber:iq:register",
+          })
+          .c("username")
+          .t(user.username)
+          .up()
+          .c("password")
+          .t(user.password);
+
+        xmpp.send(iq);
+      });
+
+      // Handle response
+      xmpp.on("stanza", (stanza) => {
+        if (stanza.is("presence")) {
+          console.log("Connection established successfully");
+        } else if (stanza.is("message") && stanza.attrs.type === "result") {
+          console.log("User registered successfully");
+        } else if (stanza.is("message") && stanza.attrs.type === "error") {
+          const errorElement = stanza.getChild("error");
+          const errorText = errorElement.getChildText("text");
+          console.error("Error registering user:", errorText);
+        }
+      });
     }
   }
 
@@ -29,10 +64,8 @@ export default class UsersController {
 
     const exists = await User.query().where({ key }).first();
 
-    if(exists){
-      xmpp.send(
-        reciver+"@"+Env.get('EJABBERD_HOST'), message);      
+    if (exists) {
+      xmpp.send(reciver + "@" + Env.get("EJABBERD_HOST"), message);
     }
-
   }
 }
